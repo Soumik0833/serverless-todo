6d13e5c2f286cd6a5675aaf65f487095e0c24fde
Soumik0833 <soumikghoshmoulic@gmail.com>
2020-05-15 21:33:43 +0200

    Added the update ToDo function

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -2,9 +2,13 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { createToDo } from '../../business_logic/businessFunctions'
+import { createLogger } from '../../utils/logger'
 
+
+const logger = createLogger('getToDo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.info('Processing the following event - ', event)
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
   let auth = event.headers.Authorization
   let split = auth.split(' ')
--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,27 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import {updateToDo} from '../../business_logic/businessFunctions'
+import { createLogger } from '../../utils/logger'
+
+
+const logger = createLogger('getToDo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.info('Processing the following event - ', event)
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  let auth = event.headers.Authorization
+  let split = auth.split(' ')
+  let jwtToken = split[1]
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  let update = await updateToDo(updatedTodo, todoId, jwtToken)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      items: update
+    })
+  }
+  
 }
