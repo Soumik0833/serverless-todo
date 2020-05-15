62da1b42569f1048adcd60b7fd6faa39f797b3e6
Soumik0833 <soumikghoshmoulic@gmail.com>
2020-05-15 21:15:51 +0200

    Added the Delete ToDo Function

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,24 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {createLogger} from '../../utils/logger'
+import {deleteToDo} from '../../business_logic/businessFunctions'
+
+const logger = createLogger('deleteToDo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
+  let todoId = event.pathParameters.todoId
+  logger.info('Processing the following event - ', event)
+  let auth = event.headers.Authorization
+  let split = auth.split(' ')
+  let jwtToken = split[1]
 
-  // TODO: Remove a TODO item by id
-  return undefined
+  let deleteToDoItem = await deleteToDo(todoId, jwtToken)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Deleted the item",
+      item: deleteToDoItem
+    })
+  }
 }
