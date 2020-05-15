0f4d0ee6e47a0109a2bc13e6f7c84fb1160617e2
Soumik0833 <soumikghoshmoulic@gmail.com>
2020-05-15 21:28:29 +0200

    Added the Get All ToDo function

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,22 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getAllToDo } from '../../business_logic/businessFunctions'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('getToDo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+  logger.info('Processing the following event - ', event)
+  let auth = event.headers.Authorization
+  let split = auth.split(' ')
+  let jwtToken = split[1]
+
+  let allToDo = await getAllToDo(jwtToken)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      items: allToDo
+    })
+  }
 }
